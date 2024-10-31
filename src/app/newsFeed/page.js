0569e904e5f39ed74b7fc1d4754e1374a67367fd
David Anthony <davywavy22@googlemail.com>
2024-10-31 09:10:27 +0000

    newsfeed component

---
+++ src/app/newsFeed/page.js
@@ -0,0 +1,10 @@
+import NewsFeedAPI from "@/components/NewsFeedAPI";
+
+export default async function NewsFeed() {
+  return (
+    <>
+      <h1>newsfeed</h1>
+      <NewsFeedAPI />
+    </>
+  );
+}
