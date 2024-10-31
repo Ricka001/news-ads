16e4aac8fa0303f57d9b82d3aee858239dedeaea
Anthony Ricketts <mra.ricketts@gmail.com>
2024-10-31 07:25:00 +0000

    verify adsense

--- .history/src/app/layout_20241031054442.js
+++ .history/src/app/layout_20241031054442.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+import Adsense from "@/components/Adsense";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,22 +21,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5206967219759359"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <Header />
-          {children}
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Adsense pId="ca-pub-5206967219759359" />
+        <Header />
+        {children}
+      </body>
+    </html>
   );
 }
