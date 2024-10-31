9b751789dd5c22836b599073cfeccbabc3149252
Anthony Ricketts <mra.ricketts@gmail.com>
2024-10-31 11:36:54 +0000

    added css

--- .history/src/components/Header_20241031112739.jsx
+++ .history/src/components/Header_20241031112739.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <>
-      <nav className="flex justify-center items-center space-x-80 text-black">
+      <nav className="flex flex-col items-center text-black">
         <Link href={"/"}>Home </Link>
         <Link href={"/about"}>About </Link>
       </nav>
